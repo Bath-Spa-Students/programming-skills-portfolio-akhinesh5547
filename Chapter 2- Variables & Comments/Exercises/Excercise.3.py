852df25db93722ed0a26f6c4396d70eea99c418f
akhinesh5547 <akhinesh028347@gmail.com>
2023-11-02 10:48:34 +0400

    Create Excercise.3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Excercise.3.py
@@ -0,0 +1,13 @@
+name = "\takhinesh ajikumar\n"
+
+print(":")
+print(name)
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+print("\nUsing strip():")
+print(name.strip())
